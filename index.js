99e258906f3d91b16d58b157e86664be69ee0eed
kinsiabdirahman <kinsiabdirahman@outlook.com>
2023-12-19 09:35:25 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+// Define the function superbowlWin
+function superbowlWin(array) {
+    // Use the find() method to search for an object with a result property equal to "W"
+    const win = array.find(obj => obj.result === "W");
+    // If a win is found, return the year property of the win object
+    // If no win is found, return undefined
+    return win ? win.year : undefined;
+  }
+  
